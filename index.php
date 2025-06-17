2ee8c5acb3777c1bfd65ae2f5f6528d85a918d2f
Gabeta Soro <gabeta.soro@gmail.com>
2025-06-17 18:07:44 +0000

    wip

--- index.php
+++ index.php
@@ -137,7 +137,7 @@ $pageDescription = 'Bienvenue sur la boutique en ligne de Longrich Côte d\'Ivoi
                         </li>
                     </ul>
                     <div class="mt-6">
-                        <a href="https://wa.me/2250707070707?text=Bonjour, je souhaite m'inscrire avec le code parrain LONGRICH-CI"
+                        <a href="https://wa.me/22578174738?text=Bonjour, je souhaite m'inscrire avec le code parrain LONGRICH-CI"
                             target="_blank"
                             class="inline-flex items-center justify-center w-full bg-longrich-green text-white px-4 py-2 rounded-md hover:bg-longrich-green-dark transition-colors">
                             <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
--- produits.php
+++ produits.php
@@ -63,7 +63,7 @@ $currentPageProducts = array_slice($products, $startIndex, $itemsPerPage);
                                 <p class="text-sm text-gray-500 line-through"><?php echo formatPrice($product['originalPrice']); ?></p>
                             <?php endif; ?>
                         </div>
-                        <a href="https://wa.me/2250707070707?text=Bonjour, je suis intéressé par le produit <?php echo urlencode($product['name']); ?>"
+                        <a href="https://wa.me/22578174738?text=Bonjour, je suis intéressé par le produit <?php echo urlencode($product['name']); ?>"
                             target="_blank"
                             class="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-longrich-green hover:bg-longrich-green-dark">
                             <svg class="w-5 h-5 mr-2" fill="currentColor" viewBox="0 0 24 24">
